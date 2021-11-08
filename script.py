facb56dd019dd90a5fa08af281a5fdf061e499cc
javier <jovier@gmail.com>
2021-11-08 16:20:41 -0500

    function form of the script to be converted to an object next

--- script.py
+++ script.py
@@ -4,9 +4,10 @@
 
 # Inports:
 import numpy as np
+import itertools as it
 
 # Inputs:
-size = [1,1,1]       # Number of times we are replicating the primitive cell
+size = [2,2,2]       # Number of times we are replicating the primitive cell
 center = [0,0,0]     # The position (x,y,z) in space where the ceneter is
 primitive_cell = [[4,0,0],[0,4,0],[0,0,4]] # The primitive cell
 
